2f02d23412c34b3e0fcadeb1696630d5a05af70b
GalansLage <angelalgasmontero@gmail.com>
2025-06-05 21:37:08 -0400

    Finish

--- AuthenticationApi/Data/DTOs/UserDTO.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace AuthenticationApi.Data.DTOs
-{
-    public class UserDTO
-    {
-        public string Name { get; set; }
-        public string Email { get; set; }
-        public string Password { get; set; }
-    }
-}
